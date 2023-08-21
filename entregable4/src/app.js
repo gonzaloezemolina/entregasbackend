177bdb65619bb4ef49f7e84eeebbc8be192892dc
Gonzalo <gonzaloezemolina@gmail.com>
2023-08-21 18:07:38 -0300

    Avance en handlebars y websocket

--- entregable4/src/app.js
+++ entregable4/src/app.js
@@ -1,18 +1,41 @@
 import express from 'express';
-import productsRouter from "../src/routes/products.Router.js"
-import cartRouter from "../src/routes/carts.Router.js"
+import viewRouter from "./routes/view.Router.js";
+import productsRouter from "./routes/products.Router.js"
+import cartRouter from "./routes/carts.Router.js"
+import { __dirname } from './utils.js';
+import { Server } from 'socket.io';
+import handlebars from "express-handlebars";
+
+
 const app = express ();
 
 const PORT = 8080
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(__dirname + "/public"))
+
+
+//Handlebars
+app.engine("handlebars", handlebars.engine())
+app.set("views", __dirname+ "/views")
+app.set("view engine", "handlebars" )
+
 
 //Rutas
-app.use("/api",productsRouter); 
+app.use("/api",productsRouter);
 app.use("/api",cartRouter)
+app.use("/",viewRouter)
 
+
+//Http
 const server = app.listen(PORT, () =>{
     console.log(`Server HTTP is listening on PORT ${server.address().port}`);
 })
-
 server.on("error", error => console.log(`Error en el servidor ${error}`))
+
+//Socket
+const socket = new Server(server)
+socket.on("connection", (socket) => {
+    console.log("Client connected, id: ", socket.id);
+})
+
---
+++ entregable4/src/routes/view.Router.js
@@ -0,0 +1,30 @@
+import { Router } from "express";
+import ProductManager from "../managers/productManager.js";
+import { __dirname } from "../utils.js";
+const router = Router();
+
+
+const pManager = new ProductManager (__dirname+ "/json/products.json");
+
+router.get("/", async (req, res) => {
+    try {
+        const listProducts = await pManager.getProducts();
+        res.render("home", {listProducts});
+    } catch (error) {
+        console.error("Error renderizando:", error);
+        res.status(500).send("Server Error");
+    }
+});
+
+
+router.get("/realtimeproducts", (req, res) => {
+    try {
+        res.render("realtimeproducts");
+    } catch (error) {
+        console.error("Error renderizando:", error);
+        res.status(500).send("Server Error");
+    }
+});
+
+
+export default router
